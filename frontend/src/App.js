4e5e3bdef980c8a9ac0244cacca97e6fb661e77c
michellelala <michelleshang@ac.c4q.nyc>
2018-02-12 12:02:04 -0500

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          <Link to='/explore'>explore</Link> {" "}
+          <Link to='/:username'>profile</Link>
+        </nav>
+
+        <h1>Instagram</h1>
+
+        <Switch>
+          
+        </Switch>
       </div>
     );
   }
