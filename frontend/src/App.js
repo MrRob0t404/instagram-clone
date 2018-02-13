a928202be1c52fab0b564a50c772cdc8dda0511f
michellelala <michelleshang@ac.c4q.nyc>
2018-02-13 16:58:29 -0500

    create Profile component - still unfinished.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,10 @@ import './App.css';
 
 import Register from './users/Register';
 import Login from './users/Login';
+import Profile from './users/Profile';
 
 class App extends Component {
-  // login = () => {
-  //   return (
-  //     <div>
-  //       <h1>Instagram</h1>
-  //     </div>
-  //   )
-  // }
+
 
   render() {
     return (
@@ -21,6 +16,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Register} />
           <Route path='/login' component={Login} />
+          <Route path='/:username' component={Profile} />
         </Switch>
       </div>
     );
--- frontend/src/users/Profile.js
+++ frontend/src/users/Profile.js
@@ -1,32 +1,87 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class Profile extends Component {
   constructor() {
-		super();
-		this.state = {
-			username: "",
-			bio: "",
-			followerCount: 0, 
-			followingCount: 0,
-			images: []
-		}
-	}
-	
-	componentDidMount() {
-		axios
-			.get('/users/something') // change later
-			.then(res => {
-				this.setState({
+    super();
+    this.state = {
+      username: "testUsername",
+      bio: "biography description goes here",
+      followerCount: 0,
+      followingCount: 0,
+      postCount: 0,
+      images: [
+        "http://cdn.akc.org/Marketplace/Breeds/Shih_Tzu_SERP.jpg",
+        "https://i.ytimg.com/vi/IslI1VaG4nM/maxresdefault.jpg",
+        "http://cdn.akc.org/Marketplace/Breeds/Pekingese_SERP.jpg"
+      ],
+      message: ""
+    };
+  }
 
-				})
-			})
-	}
+  componentDidMount() {
+    axios
+      .get(`/users/${this.state.username}/posts`)
+      .then(res => {
+        this.setState({
+          username: res.data.username,
+          bio: res.data.bio,
+          followerCount: res.data.followers.length,
+          followingCount: res.data.following.length,
+          images: res.data.images
+        });
+      })
+      .catch(err => {
+        this.setState({
+          message: `Error retrieving profile for ${this.state.username}.`
+        });
+      });
+  }
 
-	render() {
-		return (
-			<div>
-			</div>
-		)
-	}
+  render() {
+    const {
+      username,
+      bio,
+      followerCount,
+      followingCount,
+      postCount,
+      images,
+      message
+    } = this.state;
+    return (
+      <div>
+        <nav>
+					<img
+						src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-32.png"
+						alt="Instagram camera logo"
+					/>
+          <img
+            className="instaLettersProfile"
+            src="http://pngimg.com/uploads/instagram/instagram_PNG5.png"
+            alt="Instagram"
+          />
+          <Link to={`/${username}`}>{username}</Link>
+        </nav>
+
+        <div className="profileBlurb">
+          <h2>{username}</h2>
+          <p className="profileP">
+            {postCount} posts
+          </p>
+          <p className="profileP">
+            {followerCount} followers
+          </p>
+          <p className="profileP">
+            {followingCount} following
+          </p>
+          <p>{bio}</p>
+        </div>
+        <hr className="profileHR" />
+        {images.map(imgURL => {
+          return <img src={imgURL} alt="user image" className="profileImage" />;
+        })}
+      </div>
+    );
+  }
 }
