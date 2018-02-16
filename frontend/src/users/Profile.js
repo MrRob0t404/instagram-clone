c4efe1e582c395f9cf582bfe0e53dc8d8add64ee
michellelala <michelleshang@ac.c4q.nyc>
2018-02-16 10:23:05 -0500

    styling logout and add image button in Profile

--- frontend/src/users/Profile.js
+++ frontend/src/users/Profile.js
@@ -4,8 +4,8 @@ import { Link, Route } from "react-router-dom";
 
 import Logout from "./Logout";
 
-class  Profile extends React.Component {
-  constructor(props){
+class Profile extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       following: "",
@@ -13,8 +13,8 @@ class  Profile extends React.Component {
       images: [],
       addImage: false,
       newDesc: "",
-			newURL: "",
-    }
+      newURL: ""
+    };
   }
 
   componentDidMount() {
@@ -28,27 +28,27 @@ class  Profile extends React.Component {
       .catch(err => {
         console.log(err);
       });
-      axios
-        .get(`/users/followees`)
-        .then(res => {
-          this.setState({
-            following: res.data
-          });
-        })
-        .catch(err => {
-          console.log(err);
+    axios
+      .get(`/users/followees`)
+      .then(res => {
+        this.setState({
+          following: res.data
         });
-        // axios
-        //   .get(`/users`)
-        //   .then(res => {
-        //     this.setState({
-        //       images: res.data
-        //     });
-        //   })
-          // .catch(err => {
-          //   console.log(err);
-          // });
-          this.getImages()
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    // axios
+    //   .get(`/users`)
+    //   .then(res => {
+    //     this.setState({
+    //       images: res.data
+    //     });
+    //   })
+    // .catch(err => {
+    //   console.log(err);
+    // });
+    this.getImages();
   }
 
   getImages = () => {
@@ -56,135 +56,153 @@ class  Profile extends React.Component {
       .get(`/users`)
       .then(res => {
         this.setState({
-          images: res.data,
+          images: res.data
         });
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  };
 
   // -------------------- PROFILE HANDLER FUNCTIONS -------------------- //
-	handleSubmitPost = e => {
-		e.preventDefault();
-		axios
-			.post('/users/postImage', {
-				post_descrip: this.state.newDesc,
-				img: this.state.newURL
-			},
-			this.setState({
-				newDesc: "",
-				newURL: "",
-				message: "Added image",
-				addImage: false
-			}),
-      this.getImages()
-		)
-			.then(res => {
-				console.log("Added image!")
-			})
-			.catch(err => {
-				this.setState({
-					message: "Error posting new image"
-				})
-			})
-	}
+  handleSubmitPost = e => {
+    e.preventDefault();
+    axios
+      .post(
+        "/users/postImage",
+        {
+          post_descrip: this.state.newDesc,
+          img: this.state.newURL
+        },
+        this.setState({
+          newDesc: "",
+          newURL: "",
+          message: "Added image",
+          addImage: false
+        }),
+        this.getImages()
+      )
+      .then(res => {
+        console.log("Added image!");
+      })
+      .catch(err => {
+        this.setState({
+          message: "Error posting new image"
+        });
+      });
+  };
 
   handleInput = e => {
     this.setState({
       [e.target.name]: e.target.value
-    })
-  }
-	handleAddImage = e => {
+    });
+  };
+  handleAddImage = e => {
     this.setState({ addImage: !this.state.addImage });
-	};
+  };
 
   render() {
     const { user_id, username, bio } = this.props;
-    const { newDesc, newURL, followers,
-            following, images, follower,
-            addImage } = this.state;
+    const {
+      newDesc,
+      newURL,
+      followers,
+      following,
+      images,
+      follower,
+      addImage
+    } = this.state;
 
-  return (
-    <div>
-   <nav>
-     <div className="logoDiv">
-       <img
-         className="logo"
-         src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-48.png"
-         alt="Instagram camera logo"/>
-       <div className="verticalLine"/>
-       <img
-         className="instaLettersProfile"
-         src="http://pngimg.com/uploads/instagram/instagram_PNG5.png"
-         alt="Instagram"/>
-     </div>
-     {/* <input
-       className="searchbar"
-       type="text"
-       value={searchInput}
-       onChange={handleInput}
-       name="searchInput"
-       placeholder="Search"
-     /> */}
-     <Link to={`/${username}`}>{username}</Link>
-     <Logout/>
-   </nav>
+    return (
+      <div>
+        <nav>
+          <div className="logoDiv">
+            <img
+              className="logo"
+              src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-48.png"
+              alt="Instagram camera logo"
+            />
+            <div className="verticalLine" />
+            <img
+              className="instaLettersProfile"
+              src="http://pngimg.com/uploads/instagram/instagram_PNG5.png"
+              alt="Instagram"
+            />
+          </div>
+          <input
+       			className="searchbar"
+       			type="text"
+       			value=""
+       			onChange=""
+       			name="searchInput"
+       			placeholder="Search"
+     			/>
+          <Link to={`/${username}`}>{username}</Link>
+          <Logout />
+        </nav>
 
-   <div>
-     <button onClick={this.handleAddImage}>Add Image</button>
-   </div>
+        <div>
+          <button className="addImageButton" onClick={this.handleAddImage}>Add Image</button>
+        </div>
 
-   <div>
-     {addImage
-       ? (
-         <div>
-           <form onSubmit={this.handleSubmitPost}>
-             <input
-               type="text"
-               value={newURL}
-               placeholder="URL"
-               name="newURL"
-               onChange={this.handleInput}/>
-             <input
-               type="text"
-               value={newDesc}
-               placeholder="Description"
-               name="newDesc"
-               onChange={this.handleInput}/>
-             <input type="submit"/>
-           </form>
-         </div>
-       )
-       : ("")}
+        <div>
+          {addImage ? (
+            <div>
+              <form onSubmit={this.handleSubmitPost}>
+                <input
+                  type="text"
+                  value={newURL}
+                  placeholder="URL"
+                  name="newURL"
+                  onChange={this.handleInput}
+                />
+                <input
+                  type="text"
+                  value={newDesc}
+                  placeholder="Description"
+                  name="newDesc"
+                  onChange={this.handleInput}
+                />
+                <input type="submit" />
+              </form>
+            </div>
+          ) : (
+            ""
+          )}
 
-     <div className="profileBlurb">
-       <h2>{username}</h2>
-       <p className="profileP">
-         posts</p> {images.length}
-       <p className="profileP">
-         followers</p> {followers.length}
-       <p className="profileP">
-         following</p>{following.length}
-       <p>{bio}</p>
-
-       <ul>
-          {followers? followers.map( follower => {
-            return <li><Link to="/follower.username">{follower.username}</Link></li>
-          }): "no any followers yet"}
-        </ul>
-
-     </div>
-     <hr className="profileHR"/>
-      {images? images.map(imgURL => {
-         return <div><img src={imgURL.img} alt="user image" className="profileImage"/>
-         <p>{imgURL.username}: {imgURL.post_descrip}</p></div>
-       }): "No images"}
-   </div>
- </div>
-  )
-
+          <div className="profileBlurb">
+            <h2>{username}</h2>
+            <p className="profileP">{images.length} posts</p>
+            <p className="profileP">{followers.length} followers</p> 
+            <p className="profileP">{following.length} following</p>
+            <p>{bio}</p>
+            <ul>
+              {followers
+                ? followers.map(follower => {
+                    return (
+                      <li>
+                        <Link to="/follower.username">{follower.username}</Link>
+                      </li>
+                    );
+                  })
+                : "no any followers yet"}
+            </ul>
+          </div>
+          <hr className="profileHR" />
+          {images
+            ? images.map(imgURL => {
+                return (
+                  <div>
+                    <img src={imgURL.img} alt="user image" className="profileImage" />
+                    <p> <span className="boldBlackText">{imgURL.username}</span> {imgURL.post_descrip} </p>
+                  </div>
+                );
+              })
+            : "No images"}
+        </div>
+      </div>
+    );
   }
-};
+}
 
 export default Profile;
